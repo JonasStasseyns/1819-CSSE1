9e0535e746de05a17dd6a63258b5244d55cb267e
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2018-10-10 13:05:40 +0200

    Function

--- Program.cs
+++ Program.cs
@@ -8,6 +8,23 @@ namespace _1819_CSSE1
         {
             Console.WriteLine("Hello Worlds!");
             Console.WriteLine("test");
+        
+            int a = 5;
+            int b = 6;
+            int resultaat1 = optellen(a, b);
+            int resultaat2 = deling(a, b);
+            int resultaat3 = verhogen(a);
+        }
+        static int optellen(int a, int b){
+                return a+b;
+        }
+
+        static int deling(int a, int b){
+            return a/b;
+        }
+
+        static int verhogen(int a){
+            return a++;
         }
     }
 }
