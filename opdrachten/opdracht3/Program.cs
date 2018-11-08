f0e1f0352abff7b5ce0d5a5eebfde2351d76d163
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2018-11-08 22:01:31 +0100

    Opdracht 3

---
+++ opdrachten/opdracht3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace opdracht3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var menu = new(string, double)[]{
+                ("plat water", 1),
+                ("cola 25cl", 1.5),
+                ("cola 33cl", 2),
+                ("witte wijn", 3),
+                ("pils", 2),
+                ("toast", 4),
+                ("kaasplank", 5)
+            };
+
+            foreach(var element in menu){
+                Console.WriteLine(element);
+            }
+
+
+
+
+            string[,] list = new string[7, 3]{
+                {"platwater","1", "1"},
+                {"cola 25cl","1,5", "2"},
+                {"cola 33cl","2", "3"},
+                {"witte wijn","3", "4"},
+                {"pils","2", "5"},
+                {"Toast","5", "6"},
+                {"Kaasplank","4", "7"}
+            };
+
+            Console.WriteLine("Enter index");
+            string indexString = Console.ReadLine();
+            int index = int.Parse(indexString)-1;
+            Console.WriteLine(list[index, 0] + ", " + list[index, 1]);
+
+        }
+    }
+}
